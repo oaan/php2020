3f21871f001093ebd1071ca75074c3dfc20fa4c4
oscar <oaangarita@hotmail.com>
2022-09-20 07:13:38 -0300

    SSC

--- Vista/actualizar.php
+++ Vista/actualizar.php
@@ -1,7 +1,57 @@
-<?php
-cambios git
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+<html>
+    <head>
+        <title>actualizar.php</title>
+        <link rel="stylesheet" href="../miEstilo.css"/>
+    </head>
+    <body>
+        <header>
+            <?php
+            include './encabezado.php';
+            include '../Persistencia/conexion.php';
+            include '../Modelo/mod_rn.php';
+            $objCN = new conexion();
+            $objRN = new mod_rn($HCL, $FN, $NOM, $AM, $OS, $AFILIADO, $HEPB);
+            $objRN->getHCL();
+            $objCN->actualiza($objRN);
+            ?>
+        </header>
+        <section>
+            <?php
+            if (isset($_POST['btnRN'])) {
+                $boton = $_POST['btnRN'];
+                if ($boton == 'Buscar') {
+                    $rn = $objCN->busca($objRN);
+                }
+                if ($boton == 'Actualiza') {
+                    $HCL = $_POST['txtHCL'];
+                    ;
+                    $FN = $_POST['txtFN'];
+                    $NOM = $_POST['txtNOM'];
+                    $AM = $_POST['txtAM'];
+                    $OS = $_POST['txtOS'];
+                    $AFILIADO = $_POST['txtAFILIADO'];
+                    $HEPB = $_POST['txtHEPB'];
+                    $rn = new mod_rn($HCL, $FN, $NOM, $AM, $OS, $AFILIADO, $HEPB);
+                    $objCN->actualiza($objRN);
+                }
+            }
+            ?>
+            <h3>Actualizar Recien Nacidos</h3>
+            <form method="post">
+                <table>
+                    <tr>
+                        <td>Historia</td>
+                    </tr>
+                </table>
+            </form>
+        </section>
+        <footer>
+            <br>
+            <?php 
 
+            include './pie.php';
+
+?>
+        </footer>
+    </body>
+</html>
